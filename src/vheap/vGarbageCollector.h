04c7278790c5aa7be55f966d478bde6f616e2809
Pablo Rodriguez <paroque28@gmail.com>
2015-03-18 10:08:06 -0600

    Se crean clases

---
+++ src/vheap/vGarbageCollector.h
@@ -0,0 +1,14 @@
+//
+// Created by pablo on 18/03/15.
+//
+
+#ifndef _VH2015_VGARBAGECOLLECTOR_H_
+#define _VH2015_VGARBAGECOLLECTOR_H_
+
+
+class vGarbageCollector {
+
+};
+
+
+#endif //_VH2015_VGARBAGECOLLECTOR_H_
---
+++ src/vheap/vHeap.cpp
@@ -0,0 +1,4 @@
+//
+// Created by pablo on 18/03/15.
+//
+
