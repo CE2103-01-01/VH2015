8cdb3e9df6780110714a0a447988c0cfac8f42c8
alex <alexfengli23@gmail.com>
2015-03-28 19:16:37 -0600

    Socket

--- src/Socket/Socket.h
+++ src/Socket/Socket.h
@@ -91,11 +91,8 @@ void* Socket::connection_handler(void *socket_desc) {
     char *message , client_message[2000];
 
     //Send some messages to the client
-    // message = "Greetings! I am your connection handler\n";
-    //write(sock , message , strlen(message));
 
-    //message = "Now type something and i shall repeat what you type \n";
-    //write(sock , message , strlen(message));
+
 
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
