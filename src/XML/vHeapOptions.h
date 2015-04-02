b9e068ff4227c5d2094377a4fdbc64f0982a7672
roberto <rbj1504@hotmail.com>
2015-04-02 17:27:22 -0600

    Modificaciones y reacomodo

---
+++ src/XML/vHeapOptions.h
@@ -0,0 +1,28 @@
+//
+// Created by roberto on 02/04/15.
+//
+
+#ifndef VH2015_VHEAPOPTIONS_H
+#define VH2015_VHEAPOPTIONS_H
+
+#include <stdlib.h>
+#include <iostream>
+
+class vHeapOptions {
+    std::string path;
+    bool* active;
+    int* size;
+    float* overweight;
+
+    public:
+        vHeapOptions();
+        static char const *const xmlPath = "vHeap.xml";
+        ~vHeapOptions();
+        int operator[](std::string);
+        int operator[](bool);
+        int operator[](int);
+        int operator[](float);
+};
+
+
+#endif //VH2015_VHEAPOPTIONS_H
--- src/XML/xmlReader.cpp
+++ src/XML/xmlReader.cpp
@@ -1,25 +1,15 @@
-using namespace std;
 using namespace pugi;
 
-static char const *const xmlPath = "vHeap.xml";
+xmlReader::xmlReader() {};
 
-xmlReader::xmlReader() {
-}
+xmlReader::~xmlReader() {};
 
-xmlReader::~xmlReader() {
-    delete this;
-}
-
-Opciones xmlReader::vHeapOptions() {
-
+void xmlReader::read(vHeapOptions* ret) {
     xml_document doc;
-    doc.load_file(xmlPath);
+    doc.load_file(ret->xmlPath);
     xml_node raiz = doc.child("VH2015");
-
-    opciones.activo = raiz.child("vdebug").attribute("activo").as_bool();
-    opciones.path = raiz.child("vdebug").attribute("vdebug-file-path").value();
-    opciones.size = raiz.child("vHeap").attribute("size").as_int();
-    opciones.overweight = raiz.child("vHeap").attribute("overweight").as_float();
-
-    return opciones;
-}
+    ret[raiz.child("vdebug").attribute("activo").as_bool()];
+    ret[raiz.child("vdebug").attribute("vdebug-file-path").value()];
+    ret[raiz.child("vHeap").attribute("size").as_int()];
+    ret[raiz.child("vHeap").attribute("overweight").as_float()];
+};
